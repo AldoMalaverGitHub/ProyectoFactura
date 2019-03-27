b83d1cc894d40a3247cf65ddeeabb2bd9bca0655
aldoMalaverGitHub <almalaver@gmail.com>
2019-03-26 21:33:16 -0500

    proyecto factura

---
+++ src/main/java/com/java/pj/entidades/Proyecto.java
@@ -0,0 +1,13 @@
+package com.java.pj.entidades;
+
+public class Proyecto {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Aldo");
+	}
+	
+	public String devolverNombre(){
+		return "Aldo";
+	}
+	
+}
